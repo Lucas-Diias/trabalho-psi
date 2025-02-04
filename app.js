4acf149ac29e7ecdeb915e1514d5a8db6af6ed6c
Vh <victor.silva26912@al.aememmartins.pt>
2025-02-04 11:10:53 +0000

    ejs

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-const axios = require('axios');
+const express = require('express');
+const app = express()
+const port = 3001
+
+app.use(express.json());
+
+app.set( "view engine", "ejs");
+
+app.get("/", (req, res) =>  {
+
+    let msg = "A minha lista de compras é: ";
+    let ListadeCompras = ["coca", "pepsi", "Coca-Cola"];
 
 
-axios.get('https://pokeapi.co/api/v2/pokemon/dragonite')
-.then(function (response){ 
-    console.log(response.data.types[0].type.name);
-    console.log(response.data.types[1].type.name);
+
+    res.render("index", {mensagem: msg, compras: ListadeCompras});
 })
-.catch(function(error) {
-    console.log(error);
-});
-//sabi
+
+app.get("/lista", (req, res) =>  {
+
+    let msg = "A minha lista de compras é: ";
+    let ListadeCompras = ["coca", "pepsi", "Coca-Cola"];
+
+
+
+    res.render("index", {mensagem: msg, compras: ListadeCompras});
+})
+app.get("/contactos", (req, res) =>  {
+
+    let msg = "A minha lista de compras é: ";
+    let ListadeCompras = ["coca", "pepsi", "Coca-Cola"];
+
+
+
+    res.render("index", {mensagem: msg, compras: ListadeCompras});
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
